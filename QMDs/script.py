3ad303c3d480503176b7ec026f06b176499ee1c8
27Felipea <felipearredondof27@gmail.com>
2026-01-21 21:51:53 -0700

    importing matplotlib and seaborn

--- QMDs/script.py
+++ QMDs/script.py
@@ -1,3 +1,5 @@
 import pandas as pd
-ufc_data = pd.read_csv("data/ufc_champs_final.csv")
-print(ufc_data.head())
+ufc = pd.read_csv("data/ufc_champs_final.csv")
+print(ufc.head())
+# 121 columns seems like a lot need to narrow it down, how should I clean the data?
+
